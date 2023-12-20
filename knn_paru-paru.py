b09bebff29cfddf4430b3bac5a10b984efa21e6c
putriadnda19 <120323777+putriadnda19@users.noreply.github.com>
2023-12-20 21:06:23 +0700

    Add files via upload

---
+++ knn_paru-paru.py
@@ -0,0 +1,94 @@
+import pickle 
+import streamlit as st 
+
+model = pickle.load(open('knn_paru-paru.sav', 'rb'))
+
+st.title('Estimasi Pasien Yang Menderita Kanker Paru-Paru')
+
+AGE = st.number_input('Input umur pasien')
+
+genre = st.radio(
+    "Apakah pasien merokok?",
+    ["YES", "NO"],
+    index=None,
+)
+
+st.write("Anda memilih", genre)
+
+genre = st.radio(
+    "Apakah pasien jari pasien kuning?",
+    ["YES", "NO"],
+    index=None,
+)
+
+st.write("Anda memilih", genre)
+
+genre = st.radio(
+    "Apakah pasien mempunyai kecemasan berlebih?",
+    ["YES", "NO"],
+    index=None,
+)
+
+st.write("Anda memilih", genre)
+
+genre = st.radio(
+    "Apakah pasien mempunyai tekanan dari teman sebaya?",
+    ["YES", "NO"],
+    index=None,
+)
+
+st.write("Anda memilih", genre)
+
+genre = st.radio(
+    "Apakah pasien batuk-batuk?",
+    ["YES", "NO"],
+    index=None,
+)
+
+st.write("Anda memilih", genre)
+
+genre = st.radio(
+    "Apakah pasien sesak nafas?",
+    ["YES", "NO"],
+    index=None,
+)
+
+st.write("Anda memilih", genre)
+
+genre = st.radio(
+    "Apakah pasien kesulitan menelan?",
+    ["YES", "NO"],
+    index=None,
+)
+
+st.write("Anda memilih", genre)
+
+genre = st.radio(
+    "Apakah pasien nyeri dada?",
+    ["YES", "NO"],
+    index=None,
+)
+
+st.write("Anda memilih", genre)
+
+genre = st.radio(
+    "Apakah pasien mempunyai penyakit kronis?",
+    ["YES", "NO"],
+    index=None,
+)
+
+st.write("Anda memilih", genre)
+
+genre = st.radio(
+    "Apakah pasien mengi (Napas Berbunyi?",
+    ["YES", "NO"],
+    index=None,
+)
+
+st.write("Anda memilih", genre)
+
+
+predict = ''
+
+if st.button('Estimasi '):
+    st.write('Aapakah Pasien Menderita Kanker Paru-Paru?: ', predict)
